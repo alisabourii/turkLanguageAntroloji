f57fedd26c33663be184a21267d8794803d0e35b
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-05-12 11:48:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -65,12 +65,87 @@ function yaziliDonemPlay(){
                 yaziliDonemCounter = 0;}
 }
 
-var milliSound = Audio("sound/fac.mp3")
-function milliPlay(){
-        milliPlay.play();
+var halkEdebiyatDonemSound = new Audio("sounds/halkEdebiyati.mp3");
+halkDonemCounter = 0;
+function halkDonemPlay(){
+        if(halkDonemCounter == 0){
+                halkEdebiyatDonemSound.play();
+                halkDonemCounter += 1;}
+        else{
+                halkEdebiyatDonemSound.pause();
+                halkDonemCounter = 0;}
 }
-function incele(donem){
-  url = 'https://tr.wikipedia.org/wiki/'+donem;
-  window.open(url,'_blank');
 
+var divanEdebiyatDonemSound = new Audio("sounds/divanEdebiyat.mp3");
+divanDonemCounter = 0;
+function divanDonemPlay(){
+        if(divanDonemCounter == 0){
+                divanEdebiyatDonemSound.play();
+                divanDonemCounter += 1;}
+        else{
+                divanEdebiyatDonemSound.pause();
+                divanDonemCounter = 0;}
 }
+
+var tanzimatDonemSound = new Audio("sounds/tanzimatDonem.mp3");
+tanzimatDonemCounter = 0;
+function tanzimartDonemPlay(){
+        if(tanzimatDonemCounter == 0){
+                tanzimatDonemSound.play();
+                tanzimatDonemCounter += 1;}
+        else{
+                tanzimatDonemSound.pause();
+                tanzimatDonemCounter = 0;}
+}
+
+var sifSound = new Audio("sounds/ServetIFununDonem.mp3");
+sifCounter = 0;
+function sifDonemPlay(){
+        if(sifCounter == 0){
+                sifSound.play();
+                sifCounter += 1;}
+        else{
+                sifSound.pause();
+                sifCounter = 0;}
+}
+
+
+
+var fecraiDonemSound = new Audio("sounds/FecrIAtiDonemi.mp3");
+fecraiDonemCounter = 0;
+function fecraiDonemPlay(){
+        if(fecraiDonemCounter == 0){
+                fecraiDonemSound.play();
+                fecraiDonemCounter += 1;}
+        else{
+                fecraiDonemSound.pause();
+                fecraiDonemCounter = 0;}
+}
+
+var milliDonemSound = new Audio("sounds/MilliDonem.mp3");
+milliDonemCounter = 0;
+function milliDonemPlay(){
+        if(milliDonemCounter == 0){
+                milliDonemSound.play();
+                milliDonemCounter += 1;}
+        else{
+                milliDonemSound.pause();
+                milliDonemCounter = 0;}
+}
+
+var cumhuriyetDonemSound = new Audio("sounds/Cumhuriyet_Donemi.mp3");
+cumhuriyetDonemCounter = 0;
+function cumhuriyetDonemPlay(){
+        if(cumhuriyetDonemCounter == 0){
+                cumhuriyetDonemSound.play();
+                cumhuriyetDonemCounter += 1;}
+        else{
+                cumhuriyetDonemSound.pause();
+                cumhuriyetDonemCounter = 0;}
+}
+
+function searchDonem(donemAdi){
+       // url ='http://www.google.com/search?q=' + donemAdi;
+        url = "https://tr.wikipedia.org/wiki/"+donemAdi;
+        window.open(url,'_blank');
+      }
